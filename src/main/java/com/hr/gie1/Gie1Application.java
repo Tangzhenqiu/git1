b33585c8f809a4013dbb1d737b6e31581c198a2a
1 <748>
2020-03-24 19:06:58 +0800

    第一次

---
+++ src/main/java/com/hr/gie1/Gie1Application.java
@@ -0,0 +1,13 @@
+package com.hr.gie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gie1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gie1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr/gie1/Gie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.gie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gie1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
